5c4ec54c7525668190a928ed4d1a1dd9a872b836
Note <you@example.com>
2015-01-05 14:36:28 -0300

    sd

--- static/php/base.php
+++ static/php/base.php
@@ -1,22 +1,17 @@
 <?php
-$db = new SQLite3('../../entry.db');
+
+$bd = new SQLite3('entry.db');
 $sql = "SELECT id,latitud ,longitud,description,user FROM entry";
-	 
-	function disconnectDB($conexion){
-		$close = mysqli_close($conexion);
-	 	return $close;
-	}
 	 
 	function getArraySQL($sql){
 		$results = $db->query($sql);
 		$rawdata = array();
 		$i=0;
-		while($row = mysqli_fetch_array($result)){
+		while($row = $results->fetchArray())){
 			$rawdata[$i] = $row;
 			$i++;
 		}
 	 	return $rawdata;
-		disconnectDB($conexion);
 	}
 	$myArray = getArraySQL($sql);
 	echo json_encode($myArray);
